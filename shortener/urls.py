41352c2315fc9038e199a6fda59b8a7b6d40a025
Aleksandr Menshikov <menshikov.a.v@icloud.com>
2022-03-02 15:36:56 +0300

    Add short url redirect

--- shortener/urls.py
+++ shortener/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import CreateShortURL
+from .views import CreateShortURL, RedirectShortUrl
 
 router = routers.DefaultRouter()
 router.register(r'create', CreateShortURL)
 
 urlpatterns = [
-    path('api/v1/', include(router.urls))
+    path('api/v1/', include(router.urls)),
+    path('<str:url_hash>', RedirectShortUrl.as_view())
 ]
--- shortener/views.py
+++ shortener/views.py
@@ -1,4 +1,7 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, mixins
+from rest_framework.views import APIView
 
 from .models import Url
 from .serializers import UrlSerializer
@@ -7,3 +10,11 @@ from .serializers import UrlSerializer
 class CreateShortURL(mixins.CreateModelMixin, viewsets.GenericViewSet):
     queryset = Url.objects.all()
     serializer_class = UrlSerializer
+
+
+class RedirectShortUrl(APIView):
+    def get(self, request, url_hash):
+        url = get_object_or_404(Url, url_hash=url_hash)
+        long_url = url.long_url
+        return HttpResponseRedirect(long_url)
+
