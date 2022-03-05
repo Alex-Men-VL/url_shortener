e862de1413553502e5ac476a9db3cf34eee6201e
Aleksandr Menshikov <menshikov.a.v@icloud.com>
2022-03-05 17:02:04 +0300

    Add a basename to api url

--- shortener/urls.py
+++ shortener/urls.py
@@ -4,10 +4,11 @@ from rest_framework import routers
 from .views import CreateShortURLAPI, CreateShortURL, RedirectShortUrl
 
 router = routers.DefaultRouter()
-router.register(r'create', CreateShortURLAPI)
+router.register(r'create', CreateShortURLAPI, basename='create_short_link_api')
 
 urlpatterns = [
     path('api/v1/', include(router.urls)),
     path('', CreateShortURL.as_view(), name='create_short_link'),
-    path('<str:url_hash>', RedirectShortUrl.as_view(), name='long_url_redirect'),
+    path('<str:url_hash>', RedirectShortUrl.as_view(),
+         name='long_url_redirect'),
 ]
